deeb9005b111b17d0a59742f40b01622e8e369a5
mbkranz <kranz-michael@norc.org>
2022-11-04 11:21:29 -0500

    Added most up to date VLMD schema ref

--- code/healdata_utils/schemas.py
+++ code/healdata_utils/schemas.py
@@ -1,10 +1,10 @@
 from pathlib import Path
 import requests
-
+from frictionless import Schema
 
 heal = {
-    'data_dictionary': requests.get(
-        "https://raw.githubusercontent.com/HEAL/heal-metadata-schemas/"
-        "variable-level-metadata/variable-level-metadata-schema/schemas/fields.json"
-    ).json()
+    'data_dictionary': Schema(
+        "https://raw.githubusercontent.com/norc-heal/heal-metadata-schemas/"
+        "mbkranz/variable-lvl/variable-level-metadata-schema/schemas/fields.json"
+    ).to_dict()
 }
