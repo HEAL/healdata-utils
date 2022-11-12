ecdddaa40662e6c863ff434baf715bb6d28c4ec6
mbkranz <kranz-michael@norc.org>
2022-11-11 21:10:37 -0600

    moved scripts to root folder called scripts for more std convention

--- scripts/readstat-data-dictionaries.py
+++ scripts/readstat-data-dictionaries.py
@@ -13,13 +13,8 @@ for study,info in studies.items():
             if dd.get('conversiontype')=='spss':
                 input_file = ROOT_DIR/'data-dictionaries'/study/'input'/dd['file_name']
                 outputdir = input_file.parents[1]/'output'
-                jsontemplate,csvtemplate = convert_readstat(input_file,to_json=True,to_csv=True)
-
-                csvpath = outputdir/(input_file.stem.lower()+'.csv')
-                csvtemplate.to_csv(csvpath)
-
+                jsontemplate = convert_readstat(input_file)
                 jsonpath = outputdir/(input_file.stem+'.json')
-                #NOTE: temp workaround: json--> dict --> json file
                 jsonpath.write_text(
                     json.dumps({'title':dd['title'],'data_dictionary':jsontemplate},indent=4),
                 )
