6b4d87c811aadbaa0de2924f1a375c3cfcd3c855
mbkranz <kranz-michael@norc.org>
2022-11-11 22:28:56 -0600

    reorg/refactor pyreadstat module

--- src/healdata_utils/transforms/readstat/conversion.py
+++ src/healdata_utils/transforms/readstat/conversion.py
@@ -1,6 +1,6 @@
 import pyreadstat
 import pandas as pd 
-from healdata_utils.transforms.readstat.mappings import fieldmap,get_std_type
+from healdata_utils.transforms.readstat.mappings import fieldmap,typemap
 from pathlib import Path
 #TODO: add original type and translated type
 # commented out are file metadata
@@ -24,80 +24,39 @@ def read_pyreadstat(file_path,**kwargs):
 
     return read(file_path,**kwargs)
 
-def convert_meta_item_to_frame(meta_object,trg_name,meta_name):
+def get_type(meta,colname,typemap):
 
-    meta_item = getattr(meta_object,meta_name)
-    if meta_name=='column_names':
-        df = (pd.DataFrame([],index=pd.Index(meta_item,name='name'))
+    for std_name,conds in typemap.items():
+        is_type = sum([meta[key][colname]==val for key,val in conds])==len(list(conds))
+        if is_type:
+            return std_name
 
-            )
-    else:
-        df = (
-            pd.Series(meta_item)
-            .rename(trg_name)
-            .to_frame()
-            .rename_axis(index='name')
-        )
-    return df 
+def is_ordered(meta,colname):
+    return meta['variable_measure'][colname]=='nominal'
 
-def gather_metadata(meta_object,mappings):
+def to_int_if_base10(string):
+    digits = string.split('.')
+    if len(digits)==2:
+        if digits[1]=='0' and digits[0].is_numeric():
+            return digits[0]
+    
+    return string
 
-    meta_list = []
-    assert mappings.get('name')!=None,'Need to specify column_name in mappings'
+def convert_readstat(file_path,fieldmap=fieldmap):
+    _,meta = read_pyreadstat(file_path,metadataonly=True)
+    meta = meta.__dict__ #change meta container object to dictionary so easier to map
+    meta_mapped = []
+    ext = Path(file_path).suffix.replace('.','')
+    for colname in meta['column_names']:
+        meta_field = {'name':colname}
+        for new_name,map_fxn in fieldmap['pyreadstat'][ext].items():
+            if new_name=='type':
+                meta_param = map_fxn(meta,colname,typemap['pyreadstat'][ext])
+            else:
+                meta_param = map_fxn(meta,colname)
+            
+            if meta_param:
+                meta_field[new_name] = meta_param
+        meta_mapped.append(meta_field)        
 
-    for trg_name,meta_name in mappings.items():
-        meta_item_df = convert_meta_item_to_frame(meta_object,trg_name,meta_name)
-        meta_list.append(meta_item_df) 
-
-    meta_df = pd.concat(meta_list,axis=1)
-
-    # across field logic
-    meta_df.update(meta_df.apply(get_std_type,axis=1))
-    meta_df['ordered'] = meta_df['format'].apply(lambda x: True if x=='nominal' else None)
-
-    return meta_df
-
-def convert_to_csvtemplate(meta_df):
-    return (
-        meta_df
-        .applymap(lambda x: ('|'.join([f"{str(key)}={str(val)}" 
-            for key,val in x.items()])) if type(x)==dict else x)
-    )
-
-def convert_to_jsontemplate(meta_df):
-    return [
-        x.dropna().to_dict() for n,x in meta_df.reset_index().iterrows()
-    ]
-def convert_readstat(file_path,to_json=True,to_csv=False):
-    ''' 
-    reads in a pyreadstat 
-    and outputs both a json and
-    csv template
-    ''' 
-    file_path = Path(file_path)
-
-    df,meta = read_pyreadstat(file_path)
-    #field name mappings
-    mapping = fieldmap['pyreadstat'][file_path.suffix.replace('.','')]
-    meta_df = gather_metadata(meta, mapping)
-
-    if to_json:
-        json = convert_to_jsontemplate(meta_df)
-    else:
-        json = None
-
-    if to_csv:
-        csv = convert_to_csvtemplate(meta_df)
-
-
-
-    if to_json and not to_csv:
-        return json 
-
-    elif not to_json and to_csv:
-        return csv
-    else:  
-        return json,csv
-
-
-
+    return meta_mapped
--- src/healdata_utils/transforms/readstat/mappings.py
+++ src/healdata_utils/transforms/readstat/mappings.py
@@ -4,27 +4,61 @@ for a variety of input formats
 '''
 from pathlib import Path
 
-# TODO: make type mappings 
-def get_std_type(row):
+def get_type(meta,colname,typemap):
 
-    if row['format']=='scale' and row['type']=='double':
-        type_std = 'number'
-    elif row['format']=='nominal' and row['type']=='double':
-        type_std = 'integer'
+    for std_name,conds in typemap.items():
+        is_type = sum([meta[key][colname]==val for key,val in conds.items()])==len(list(conds))
+        if is_type:
+            return std_name
+
+def is_ordered(meta,colname):
+    return meta['variable_measure'].get(colname,'')=='nominal'
+
+def to_int_if_base10(val):
+
+    # if numeric
+    if type(val)==int:
+        return str(val)
+    elif type(val)==float:
+        if val.is_integer():
+            return str(int(val))
+        else:
+            return str(val)
+
+    #could be a string representation of number
     else:
-        type_std = 'string'
-    
-    row['type'] = type_std
-    return row
+        digits = str(val).split('.')
+        if len(digits)==2:
+            if digits[1]=='0' and digits[0].is_numeric():
+                return digits[0]
+        
+    return str(val)
+
+typemap = {
+    'pyreadstat':{
+        'sav':{
+            'number':{'readstat_variable_types':'double','variable_measure':'scale'},
+            'integer':{'readstat_variable_types':'double','variable_measure':'nominal'},
+            'string':{'readstat_variable_types':'string'}
+        }
+    }
+}
 
 fieldmap = {
     'pyreadstat':{
         'sav':{
-            'name':'column_names',
-            'type':'readstat_variable_types',
-            'format':'variable_measure', #TODO: capture original_variable_type
-            'description':'column_names_to_labels',
-            'encodings':'variable_value_labels'
+            'type': lambda meta,colname,typemap: get_type(meta,colname,typemap),
+            #'format':lambda meta,colname: meta['variable_measure'][colname], #TODO: capture original_variable_type
+            'description':lambda meta,colname: meta['column_names_to_labels'][colname],
+            'encodings':lambda meta,colname: {
+                to_int_if_base10(key):to_int_if_base10(val) 
+                for key,val in meta['variable_value_labels'].get(colname,{}).items()
+            },
+            'ordered': lambda meta,colname: is_ordered(meta,colname)
         }
     }
 }
+
+
+
+
