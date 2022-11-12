3fb036ed1e050752ab84661f4e84f8220feec221
mbkranz <kranz-michael@norc.org>
2022-11-11 23:00:58 -0600

    Added json to csv as option for CLI; related refactor and reorg

--- src/healdata_utils/cli.py
+++ src/healdata_utils/cli.py
@@ -2,7 +2,8 @@
 
 command line interface for generating HEAL data dictionary/vlmd json files
 
-TODO: refactor top level data dictionary values and validation 
+##(TODO: need to add required title directly to schema and validation 
+## add data dictionary level fields
 ''' 
 
 
@@ -16,14 +17,22 @@ import healdata_utils.schemas as schemas
 
 choice_fxn = {
     'csvtemplate': convert_template_csv_to_json,
-    'spss': convert_readstat
+    'spss': convert_readstat,
+    'json':convert_template_json_to_csv
 }
 
 #TODO: make scheam a CLI option?
 schema = schemas.heal['data_dictionary']
 
+def generate_outputpath(input_filepath,outdir,suffix='json'):
+    outputdir_is_file = len(outputdir.suffixes)>0
+    if outputdir_is_file:
+        outputpath = outputdir
+    else:
+        outputpath = outputdir/input_filepath.with_suffix(suffix).name
+    return outputpath 
 
-def main(filepath,title,description,inputtype,outputdir):
+def to_json(filepath,title,description,inputtype,outputdir):
     ''' 
     write a data dictioanry (ie variable level metadata)
     to a HEAL metadata json file
@@ -31,19 +40,15 @@ def main(filepath,title,description,inputtype,outputdir):
     outputdir = Path(outputdir)
     filepath = Path(filepath)
 
+    outputdir.mkdir(exist_ok=True)
+
     # get data dictionary based on the input type
-    # NOTE: the spss function is temporary -- will only output a python dict in future but put this 
-    # in for demo purpo9se
     dd_fields = choice_fxn[inputtype](filepath)
 
-    #TODO: pull down to validate entrie data dict
-    schemas.validate(dd_fields)
-
     ## add top level data dictionary information
-    data_dictionary = {}
+    data_dictionary = {'data_dictionary':dd_fields}
 
-    ##(TODO: need to add required title directly to schema and validation 
-    ## add data dictionary level fields
+
     if description:
         data_dictionary['description'] = description
 
@@ -53,19 +58,34 @@ def main(filepath,title,description,inputtype,outputdir):
     else:
         data_dictionary['title'] = filepath.stem
 
-    data_dictionary['data_dictionary'] = dd_fields
+    # VALIDATE
+    print('Validating output json file.....')
+    validate_json(data_dictionary)
 
     #Output jsonfile
-    outputdir_is_file = len(outputdir.suffixes)>0
-    if outputdir_is_file:
-        outputpath = outputdir
-    else:
-        outputpath = outputdir/filepath.with_suffix('json').name
+    outputpath = generate_outputpath(filepath, outdir,'json')
 
     with open(outputpath,'w') as f:
         json.dump(data_dictionary,f,indent=4)
 
-        
+def to_csv_from_json(filepath,outputdir):
+    ''' 
+    converts a json file to a csv
+    ''' 
+    Path(outdir).mkdir(exists_ok=True)
+    resource = choice_fxn['json'](filepath)
+    # VALIDATE
+    print('Checking input json file.....')
+    validate_json(data_dictionary)
+
+    outputpath = generate_outputpath(filepath, outputdir,'csv')
+
+    # write data dictionary params and path to CSV templates
+    resource.to_yaml(Path(outputpath).with_suffix('yaml'))
+
+    # write data dictionary fields
+    resource.to_petl().tocsv(outputpath)
+
 @click.command()
 @click.option('--filepath',required=True,help='Path to the file you want to convert to a HEAL data dictionary')
 @click.option('--title',default=None,help='The title of your data dictionary. If not specified, then the file name will be used')
@@ -77,42 +97,10 @@ def main(filepath,title,description,inputtype,outputdir):
     write a data dictioanry (ie variable level metadata)
     to a HEAL metadata json file
     '''
-    outputdir = Path(outputdir)
-    filepath = Path(filepath)
-
-    # get data dictionary based on the input type
-    # NOTE: the spss function is temporary -- will only output a python dict in future but put this 
-    # in for demo purpo9se
-    dd_fields = schemas.validate(choice_fxn[inputtype](filepath))
-
-    #TODO: pull down to validate entrie data dict
-
-    ## add top level data dictionary information
-    data_dictionary = {}
-
-    ##(TODO: need to add required title directly to schema and validation 
-    ## add data dictionary level fields
-    if description:
-        data_dictionary['description'] = description
-
-    ## add dd title
-    if title:
-        data_dictionary['title'] = title
+    if inputtype=='json':
+        to_csv_from_json(filepath, outputdir)
     else:
-        data_dictionary['title'] = filepath.stem
-
-    data_dictionary['data_dictionary'] = dd_fields
-
-    #Output jsonfile
-    outputdir_is_file = len(outputdir.suffixes)>0
-    if outputdir_is_file:
-        outputpath = outputdir
-    else:
-        outputpath = outputdir/filepath.with_suffix('json').name
-
-    with open(outputpath,'w') as f:
-        json.dump(data_dictionary,f,indent=4)
-
-
+        to_json(filepath,title,description,inputtype,outputdir)
+  
 if __name__=='__main__':
     main()
--- src/healdata_utils/schemas.py
+++ src/healdata_utils/schemas.py
@@ -1,6 +1,11 @@
 from pathlib import Path
 from frictionless import Schema
 import jsonschema
+
+# TODO: use data_dictionary.json 
+# TODO: output informative error messages in validation
+# NOTE: would it be good to also have a frictionless CSV template with regexs?...may be easier to spot text errors?
+
 heal = {
     'data_dictionary': Schema(
         "https://raw.githubusercontent.com/norc-heal/heal-metadata-schemas/"
@@ -8,14 +13,23 @@ heal = {
     ).to_dict()
 }
 
-schema = heal['data_dictionary']
-def validate(dd_fields):
+schema = {
+    'type':'object',
+    'required':[
+        'title',
+        'data_dictionary'
+    ],
+    'properties':{
+        'title':{'type':'string'},
+        'description':{'type':'string'},
+        'data_dictionary':{'type':'array','items':heal['data_dictionary']}
+    }
+}
+
+def validate_json(data_dictionary,schema=schema):
     ''' 
     Validates the data dictionary fields property (need to add outermost (see TODOs below))
 
     ''' 
-    # TODO: use jsonschema resolver and data_dictionary.json 
-    # TODO: output informative error messages
-    # TODO: add required property of title and optional of description to data_dictionary.json schema
 
-    return jsonschema.validate(dd_fields,schema={'type':'array','items':schema})
+    jsonschema.validate(data_dictionary,schema=schema)
