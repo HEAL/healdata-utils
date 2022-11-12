55793a7255d5557f9e18943182b22f5101da0a91
mbkranz <kranz-michael@norc.org>
2022-11-12 04:51:35 -0600

    add csv to json and default inputtype based on extension

--- src/healdata_utils/cli.py
+++ src/healdata_utils/cli.py
@@ -2,43 +2,44 @@
 
 command line interface for generating HEAL data dictionary/vlmd json files
 
-##(TODO: need to add required title directly to schema and validation 
-## add data dictionary level fields
+#TODO: make scheam a CLI option?
 ''' 
 
 
 import click 
 from healdata_utils.transforms.csvtemplate.conversion import convert_template_csv_to_json
+from healdata_utils.transforms.csvtemplate.conversion import convert_json_to_template_csv
 from healdata_utils.transforms.readstat.conversion import convert_readstat
 import json
 from pathlib import Path
 import jsonschema
-import healdata_utils.schemas as schemas
+from healdata_utils.schemas import validate_json
 
 choice_fxn = {
-    'csvtemplate': convert_template_csv_to_json,
-    'spss': convert_readstat,
-    'json':convert_template_json_to_csv
+    'csv': convert_template_csv_to_json,
+    'sav': convert_readstat,
+    'json':convert_json_to_template_csv
 }
 
-#TODO: make scheam a CLI option?
-schema = schemas.heal['data_dictionary']
+def generate_outputpath(input_filepath,outputdir,suffix='json'):
 
-def generate_outputpath(input_filepath,outdir,suffix='json'):
+
     outputdir_is_file = len(outputdir.suffixes)>0
     if outputdir_is_file:
         outputpath = outputdir
     else:
-        outputpath = outputdir/input_filepath.with_suffix(suffix).name
+        outputpath = Path(outputdir)/input_filepath.with_suffix(f".{suffix}").name
     return outputpath 
 
-def to_json(filepath,title,description,inputtype,outputdir):
+def to_json(filepath,outputdir,title=None,description=None,inputtype=None,):
     ''' 
     write a data dictioanry (ie variable level metadata)
     to a HEAL metadata json file
     '''
     outputdir = Path(outputdir)
     filepath = Path(filepath)
+    if not inputtype:
+        inputtype = Path(filepath).suffix.replace('.','')
 
     outputdir.mkdir(exist_ok=True)
 
@@ -49,12 +50,15 @@ def to_json(filepath,title,description,inputtype,outputdir):
     data_dictionary = {'data_dictionary':dd_fields}
 
 
+
     if description:
         data_dictionary['description'] = description
 
     ## add dd title
     if title:
         data_dictionary['title'] = title
+    elif data_dictionary.get('title'):
+        pass 
     else:
         data_dictionary['title'] = filepath.stem
 
@@ -63,7 +67,7 @@ def to_json(filepath,title,description,inputtype,outputdir):
     validate_json(data_dictionary)
 
     #Output jsonfile
-    outputpath = generate_outputpath(filepath, outdir,'json')
+    outputpath = generate_outputpath(filepath, outputdir,'json')
 
     with open(outputpath,'w') as f:
         json.dump(data_dictionary,f,indent=4)
@@ -90,13 +94,18 @@ def to_csv_from_json(filepath,outputdir):
 @click.option('--filepath',required=True,help='Path to the file you want to convert to a HEAL data dictionary')
 @click.option('--title',default=None,help='The title of your data dictionary. If not specified, then the file name will be used')
 @click.option('--description',default=None,help='Description of data dictionary')
-@click.option('--inputtype',default='csvtemplate',type=click.Choice(list(choice_fxn.keys())),help='The type of your input file.')
+@click.option('--inputtype',default=None,type=click.Choice(list(choice_fxn.keys())),help='The type of your input file.')
 @click.option('--outputdir',help='The folder where you want to output your HEAL data dictionary')
 def main(filepath,title,description,inputtype,outputdir):
     ''' 
     write a data dictioanry (ie variable level metadata)
     to a HEAL metadata json file
     '''
+    print(inputtype)
+
+    if not outputdir:
+        outputdir = Path(filepath).parent.parent/'output'
+
     if inputtype=='json':
         to_csv_from_json(filepath, outputdir)
     else:
