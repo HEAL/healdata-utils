f05f7cacec799ffa9a4a507573b8ea2221c1afd2
mbkranz <kranz-michael@norc.org>
2023-02-08 00:02:40 -0600

    refactored mappings to function

--- src/healdata_utils/transforms/redcapcsv/conversion.py
+++ src/healdata_utils/transforms/redcapcsv/conversion.py
@@ -25,33 +25,54 @@ def read(file_path):
     )
     return sourcedf.to_dict(orient="records")
 
-def gather(redcapcsv_dictionary):
+def gather(sourcefields):
     """ 
     maps and translates fields based on redcap field type
     to heal json
     """ 
-    sourcefields = sourcedf.to_dict(orient="records")
+    def _add_description(sourcefield,targetfield):
+
+        fieldlabel = sourcefield.get("label","")
+        if sourcefield.get("section"):
+            fieldsection = sourcefield['section']+": "
+        else:
+            fieldsection = ""
+        
+        
+        return fieldsection+fieldlabel+targetfield.get("description","")
+
+    def _add_title(sourcefield,targetfield):
+        if sourcefield.get("label"):
+            return sourcefield["label"]
+    
+    def _add_module(sourcefield,targetfield):
+        if sourcefield.get("form"):
+            return sourcefield["form"]
+
     targetfields = [{"name":field["name"]} for field in sourcefields]
+
     for i,sourcefield in enumerate(sourcefields):
         targetfield = targetfields[i]
         sourcefieldtype = sourcefield["type"]
-        targetfield.update(typemappings[sourcefieldtype](sourcefield))
 
-        fielddescription = ""
-        if sourcefield.get("section"):
-            fielddescription+=sourcefield['section']+": "
-            fielddescription+=sourcefield.get("label","")
-            targetfield["description"] = fielddescription+targetfield.get("description","")
-        
-        if sourcefield.get("label"):
-            targetfield["title"] = sourcefield["label"]
-    
+        if sourcefieldtype in list(typemappings):
+            mappedfield = typemappings[sourcefieldtype](sourcefield)
+            mappedfieldlist = [mappedfield] if isinstance(mappedfield,dict) else mappedfield
+            #NOTE if one sourcefield generates more than 1 target field (ie checkbox) need to iterate through
+            for mappedfield in mappedfieldlist:
+                mappedfield["description"] = _add_description(sourcefield, mappedfield)
+                mappedfield["title"] = _add_title(sourcefield, mappedfield)
+                mappedfield["module"] = _add_module(sourcefield, mappedfield)
+                targetfield.update(mappedfield)
+        else:
+            targetfields.pop(i) 
+
     return targetfields
 
 def convert_redcapcsv(file_path,
     data_dictionary_props={}):
     sourcefields = read(file_path)
-    targetfields = gather(redcapcsv_dict)
+    targetfields = gather(sourcefields)
 
     data_dictionary = data_dictionary_props.copy()
     data_dictionary['data_dictionary'] = targetfields
